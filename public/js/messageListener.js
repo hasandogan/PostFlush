b677149ec532428f1870c20155f482f6f4289dc1
Behçet Merttürk <bmertturk@gmail.com>
2022-09-29 00:43:34 +0300

    FE-1 türlü temizlik

--- public/js/messageListener.js
+++ public/js/messageListener.js
@@ -1,50 +1,42 @@
-    const url = new URL('http://3.232.144.165:8181/.well-known/mercure');
-    // Add topic to listen to
-    url.searchParams.append('topic', 'live_message');
-    /*
-    const eventSource = new EventSource(url);
-    eventSource.onmessage = event => {
-    console.log("Without Credential:");
-    console.log(event);
-    triggerElement(event)
-}
-    */
+const url = new URL('http://localhost:8181/.well-known/mercure');
+// Add topic to listen to
+url.searchParams.append('topic', 'live_message');
 
-    const es = new EventSourcePolyfill(url, {
+const es = new EventSourcePolyfill(url, {
     headers: {
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOlsibGl2ZV9tZXNzYWdlIl0sInN1YnNjcmliZSI6WyJsaXZlX21lc3NhZ2UiXX19.ZExNcHj3ehEyAQXbNv70JW8e_qX62KCEFLoIwQMPMwI' ,
-}
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOlsibGl2ZV9tZXNzYWdlIl0sInN1YnNjcmliZSI6WyJsaXZlX21lc3NhZ2UiXX19.ZExNcHj3ehEyAQXbNv70JW8e_qX62KCEFLoIwQMPMwI',
+    }
 });
-    es.withCredentials = true;
+es.withCredentials = true;
 
-    es.onmessage = message => {
-    triggerElement(message)
+es.onmessage = message => {
+    const data = JSON.parse(message.data);
+    let allData = data.username + " : " + data.message;
+    createBubble(allData);
 }
 
-    function triggerElement(message) {
-    const data = JSON.parse(message.data);
-    let allData =  data.username + " : "  + data.message;
-    createBubble(allData)
-    }
-    function createBubble(test){
-
-    var element = document.createElement("div");
+function createBubble(test) {
+    let element = document.createElement("div");
     element.classList.add('bubble');
-        element.style.position = "absolute";
-        element.style.left = getRndInteger(0, 1600)+'px';
-        element.style.top = getRndInteger(0, 500)+'px';
     element.appendChild(document.createTextNode(test));
-    document.getElementById('border').appendChild(element);
-    setTimeout(() => {  element.parentNode.removeChild(element); }, 8000);
-        document.getElementById('test').addEventListener('submit', () => {
-            document.getElementById('message').value = '';
-        });
-        document.getElementById("button").disabled = true;
-        setTimeout(function(){document.getElementById("button").disabled = false;},5000);
-    }
+    document.querySelector('#border').appendChild(element);
+    setTimeout(_ => {
+        element.parentNode.removeChild(element);
+    }, 8000);
+    document.querySelector("#button").disabled = true;
+    setTimeout(_ => {
+        document.querySelector("#button").disabled = false;
+    }, 5000);
+}
 
-
-
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
-    }
+function fetchData() {
+    const data = `username=${document.querySelector('#username').value}&message=${document.querySelector('#message').value}`;
+    fetch("message", {
+        method: 'POST',
+        body: data,
+        headers : {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",                                                                                                
+            "Access-Control-Origin": "*"
+        }
+    });
+}
