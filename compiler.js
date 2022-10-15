fcdd67b0b9347be466ee7da0d8889401ce5d0bc4
Behçet Merttürk <bmertturk@gmail.com>
2022-10-15 14:19:34 +0300

    FE-1 js configleri

---
+++ compiler.js
@@ -0,0 +1,56 @@
+const startTime = new Date();
+const { minify } = require("terser");
+const babel = require("@babel/core");
+const sass = require("sass");
+const mergerConfig = require("./merger.config");
+fs = require('fs');
+const files = [];
+
+if (!fs.existsSync(mergerConfig.outputPath)){
+	fs.mkdirSync(mergerConfig.outputPath);
+}
+
+if(mergerConfig.renderCss) {
+	if(mergerConfig.scssSource?.length > 0) {
+		fs.writeFileSync(
+			`${mergerConfig.outputPath}/${mergerConfig.outputCssFile}`, sass.compile(mergerConfig.scssSource, {
+				style: "compressed"
+			}).css,
+			'utf8'
+		);
+	}
+}
+
+if(mergerConfig.renderJs) {
+	if(mergerConfig.files?.modules?.length > 0) {
+		for(let i = 0;i < mergerConfig.files.modules.length;i++) {
+			files.push(babel.transformSync(fs.readFileSync(`${mergerConfig.modulesPath}/${mergerConfig.files.modules[i]}`, 'utf8'), {
+				"presets": [
+					require("@babel/preset-env")
+				]
+			}).code);
+		}
+	}
+	
+	if(mergerConfig.files?.custom?.length > 0) {
+		for(let i = 0;i < mergerConfig.files.custom.length;i++) {
+			files.push(babel.transformSync(fs.readFileSync(`${mergerConfig.jsSource}/${mergerConfig.files.custom[i]}`, 'utf8'), {
+				"presets": [
+					require("@babel/preset-env")
+				]
+			}).code);
+		}
+	}
+	
+	if(files.length > 0) {
+		minify(files, {
+			format: {
+				beautify: false
+			}
+		}).then(result => {
+			fs.writeFileSync(`${mergerConfig.outputPath}/${mergerConfig.outputJsFile}`, result.code, 'utf8');
+		});
+	}	
+}
+
+console.log(`Rendered at ${(new Date() - startTime) / 1000}`);
---
+++ merger.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	modulesPath: "node_modules",
+	outputPath: "dist",
+	scssSource: "assets/scss/style3.scss",
+	outputCssFile: "style3.css",
+	renderJs: false,
+	renderCss: true,
+	jsSource: "assets/javascript",
+	outputJsFile: "main.js",
+	files: {
+		"custom": [
+			"app.js"
+		]
+	}
+}
